49253309597b283ea9943aaf09638a9e9df49804
tuanthptlx1 <nt7304398@gmail.com>
2024-05-25 19:22:12 +0700

    sửa

--- js/skin.js
+++ js/skin.js
@@ -10,3 +10,28 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+/*chuc nang trinh chieu*/
+
+var slideIndex = 0;
+var slides = document.querySelectorAll(".slide img");
+var slideContainer = document.querySelector(".slide");
+var timer;
+
+function showSlides() {
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  } else if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+  }
+  slideContainer.style.transform = `translateX(-${slideIndex * 100 / slides.length}%)`;
+}
+
+function changeSlide(n) {
+  slideIndex += n;
+  clearTimeout(timer);
+  showSlides();
+  timer = setTimeout(function() { changeSlide(1); }, 3000);
+}
+
+timer = setTimeout(function() { changeSlide(1); }, 3000);
+showSlides();
